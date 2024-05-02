18cefc77d7cfa956d485617d2057966c949e49bd
joho2022 <josama1997@naver.com>
2024-05-02 13:13:07 +0900

    feat: 로그인 화면

---
+++ KTV_Practice/KTV_Practice/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  KTV_Practice
+//
+//  Created by 조호근 on 5/2/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.loginButton.layer.cornerRadius = 19
+        self.loginButton.layer.borderColor = UIColor(named: "main-brown")?.cgColor
+        self.loginButton.layer.borderWidth = 1
+    }
+
+    @IBAction func buttonDidTap(_ sender: Any) {
+    }
+    
+}
+
