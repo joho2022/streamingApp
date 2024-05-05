dbf1094cd5b0cc7b54904832dae8021466cdff6f
joho2022 <josama1997@naver.com>
2024-05-05 15:29:39 +0900

    feat: 홈헤더 셀 생성

---
+++ KTV_Practice/KTV_Practice/Home/Cell/HomeHeaderCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeHeaderCell.swift
+//  KTV_Practice
+//
+//  Created by 조호근 on 5/5/24.
+//
+
+import UIKit
+
+class HomeHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
